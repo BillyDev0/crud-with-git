3ab68f24763aaeee414cc572583103ac3afa74cb
BillyDev0 <haloi10987@gmail.com>
2026-05-10 20:19:05 +0700

    membuat fitur Read Data

---
+++ CRUD_D5/Read.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+
+url='https://jsonplaceholder.typicode.com/posts'
+file='CRUD_D5/Data.csv'
+
+res=requests.get(url)
+df=pd.DataFrame(res.json())
+
+df.to_csv(file)
+while True:
+    id=input('Masukan id: ')
+    if id:
+        try:
+            id=int(id)
+        except:
+            print('input id harus angka')
+            continue
+        cek_id=df[df['id']==id]
+        if not cek_id.empty:
+            print(cek_id)
+            break
+        else:
+            print('Data tidak ditemukan')
+    else:
+        print('input id kosong')
+        
