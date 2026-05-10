7bd2288f9b012b436ddd673fc438c43f06c5a91c
BillyDev0 <haloi10987@gmail.com>
2026-05-10 14:01:15 +0700

    membuat function main()

--- CRUD_D5/main.py
+++ CRUD_D5/main.py
@@ -3,5 +3,25 @@ def menu():
 1. Create Data
 2. Read Data
 3. Update Data
-4. Delete Data''')
+4. Delete Data
+5. Exit''')
     return input('Pilih: ')
+
+def main():
+    while True:
+        pilihan_menu=menu()
+        if pilihan_menu == "1":
+            pass
+        elif pilihan_menu == "2":
+            pass
+        elif pilihan_menu =="3":
+            pass
+        elif pilihan_menu == "4":
+            pass
+        elif pilihan_menu == "5":
+            print('Program selesai')
+            break
+        else:
+            continue
+
+main()
