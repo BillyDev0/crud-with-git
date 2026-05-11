e13d5237d05b084c3093cda8e86e20d8d314d0fc
BillyDev0 <haloi10987@gmail.com>
2026-05-11 20:27:25 +0700

    memperbaiki struktur fitur update

--- CRUD_D5/Update.py
+++ CRUD_D5/Update.py
@@ -1,5 +1,5 @@
-import requests
 import pandas as pd
+import requests
 
 url='https://jsonplaceholder.typicode.com/posts'
 file='CRUD_D5/Data.csv'
@@ -8,6 +8,7 @@ res=requests.get(url)
 df=pd.DataFrame(res.json())
 
 df.to_csv(file,index=False)
+
 while True:
     id=input('Masukan id: ')
     if id:
@@ -16,47 +17,41 @@ while True:
         except:
             print('input id harus angka')
             continue
-
-        cek_id=df['id']==id
-
-        if cek_id.any():
-            input_userId=input('Masukan userId (enter jika tidak): ')
-            input_title=input('Masukan title (enter jika tidak): ')
-            input_body=input('Masukan body (enter jika tidak): ')
-            
-            data={}
-            try:
-               if input_userId:
-                  input_userId=int(input_userId)
-                  data['userId']=input_userId
-               
-               if input_title:
-                   data['title']=input_title
-               
-               if input_body:
-                   data['body']=input_body
-               
-               if data:
-                   res=requests.patch(f'{url}/{id}',json=data)
-                   if res.status_code<400:
-                       print('Berhasil diupdate')
-                       res=requests.get(url)
-                       df=pd.DataFrame(res.json())
-                       df.to_csv(file,index=False)
-                       
-                   else:
-                       print('Data gagal diupdate')
-                   break
-            except:
-                print('input userId dan id harus angka')
         
+        cek=df['id']==id
+        if (df['id']==id).any():
+            userId=input('Masukan usereId: ')
+            title=input('Masukan title: ')
+            body=input('Masukan body: ')
+
+            data={}
+            if userId:
+                try:
+                    userId=int(userId)
+                except:
+                    print('input userId harus angka')
+                    continue
+                data['userId']=userId
+            
+            if title:
+                data['title']=title
+            
+            if body:
+                data['body']=body
+            
+            if data:
+                res=requests.patch(f'{url}/{id}',json=data)
+                if res.status_code < 400:
+                    print('Data berhasil diupdate')
+                    res=requests.get(url)
+                    df=pd.DataFrame(res.json())
+                    df.to_csv(file,index=False)
+                    
+                else:
+                    print('Data tidak dapat diupdate ke server')
+
+                break
         else:
             print('Data tidak ditemukan')
     else:
         print('input id kosong')
-
-               
-            
-            
-
-            
